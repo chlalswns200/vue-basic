24806c89d0a8bf61326df2944fb2ccdd906adb38
choiminjun <chlalswns200@naver.com>
2026-05-09 16:28:00 +0900

    3-1 데이터보간법

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div>
+    <h1>프로그래밍 프레임 워크: {{ language }}</h1>
+    <h1>Vue를 공부하자</h1>
+    <h2>콘텐츠: {{ content }}</h2>
+    <h3>작성자: {{author}}</h3>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      language: "Vue.js",
+      content: "데이터 보관하기",
+      author: "홍길동"
+    };
+  },
+};
+</script>
