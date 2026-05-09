6c7e64dde75c8d12203859ac131be1bea8a59b45
choiminjun <chlalswns200@naver.com>
2026-05-09 23:41:12 +0900

    3-3 조건부 렌더링

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <h1 v-bind:class="className">이렇게 사용해야 해요!</h1>
-    <h1 v-bind:class="{active: isActive}">이렇게 사용해야 해요!</h1>
+    <h1>isVisible 값: {{ isVisible }}</h1>
+    <h1 v-if="isVisible">
+      이 문장은 isVisible 값은 {{ isVisible }}일 경우에만 보입니다.
+    </h1>
+
+    <button @click="toggleVisibility">버튼</button>
   </div>
 </template>
 
@@ -9,18 +13,13 @@
 export default {
   data() {
     return {
-        className: "title",
-        isActive: true,
+      isVisible: false,
     };
+  },
+  method: {
+    toggleVisibility() {
+      this.isVisible = !this.isVisible;
+    },
   },
 };
 </script>
-
-<style scoped>
-h1.active{
-    color: green;
-}
-.title{
-    color: orange;
-}
-</style>
