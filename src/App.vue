55c8771ca82fd1375b1c37d3c61f90bd15abc9e8
choiminjun <chlalswns200@naver.com>
2026-05-10 14:31:37 +0900

    3-4 반복문

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-    <h1>점수는?</h1>
-    <h1 v-if="score > 90">Excellent!!</h1>
-    <h1 v-else-if="score > 75">Good!</h1>
-    <h1 v-else>Needs Improvement.</h1>
+    <h1>과일 반복문</h1>
+    <ui>
+      <li v-for="item in items">{{ item.fruit }} - {{ item.price }}</li>
+    </ui>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      score: 95
+      items:[
+        {fruit: "딸기" , price:"1,000원"},
+        {fruit: "사과" , price:"500원"},
+        {fruit: "바나나" , price:"3,000원"},
+        {fruit: "수박" , price:"1,5000원"}
+      ]
     };
   }
 };
