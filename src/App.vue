76178ff0514472fd6c3042f2c6df2e55d27a6f52
choiminjun <chlalswns200@naver.com>
2026-05-10 15:28:04 +0900

    3-5 이벤트 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,18 @@
 <template>
-  <h1>과일 반복문</h1>
-  <ul>
-    <li v-for="item in items" :key="item.id">
-      {{ item.fruit }} - {{ item.price }}
-    </li>
-  </ul>
-  <ul>
-    <li v-for="(skill, index) in skills" :key="index">
-      {{ skill }}
-    </li>
-  </ul>
+  <h1>버튼 이벤트</h1>
+  <button @click="handleClick">버튼</button>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      items: [
-        { id: 1, fruit: "딸기", price: "1,000원" },
-        { id: 2, fruit: "사과", price: "500원" },
-        { id: 3, fruit: "바나나", price: "3,000원" },
-        { id: 4, fruit: "수박", price: "15,000원" },
-      ],
-      skills: [
-        "HTML","CSS","Vue.js"
-      ]
-    };
+    return {};
   },
+  methods:{
+    handleClick(event){
+      console.log(event);
+      console.log("버튼이 클릭");
+    }
+  }
 };
 </script>
