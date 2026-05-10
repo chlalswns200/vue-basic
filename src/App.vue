48f7324bce55a66bf3d66ac63e1e3539015d0f91
choiminjun <chlalswns200@naver.com>
2026-05-10 21:02:05 +0900

    3-7 컴퓨트2

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,34 @@
 <template>
-  <h1>{{ fullName }}</h1>
-  <button @click="remainName">이름 초기화</button>
-  <button @click="changeName">이름 변경</button>
+  <input v-model="contactInfo" type="text">
+  <h3>이메일: {{ email }}</h3>
+  <h3>휴대전화: {{ phone }}</h3>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      firstName: "홍",
-      lastName: "길동"
+      email: "example@example.com",
+      phone: "010-1234-5678"
     };
   },
   computed: {
-    fullName() {
-      console.log("Compute 호출")
-      return `${this.firstName} ${this.lastName}`
-    }
-  },
-  methods: {
-    remainName(){
-      console.log("버튼1");
-      this.firstName = "홍";
-    },
-    changeName(){
-      console.log("버튼2")
-      this.firstName = "김";
+    contactInfo: {
+      get(){
+        return `이메일: ${this.email}, 휴대전화: ${this.phone}`;
+      },
+      set(newValue){
+        const parts = newValue.split(",").map((part)=> part.trim());
+        console.log(parts.length);
+
+        if(parts.length===2){
+          const emailPart = parts.find((p)=>p.startsWith("이메일:"));
+          const phonePart = parts.find((p)=> p.startsWith("휴대전화:"));
+
+          if(emailPart) this.email = emailPart.replace("이메일:","").trim();
+          if(phonePart) this.phone = phonePart.replace("휴대전화:","").trim();
+        }
+      }
     }
   }
 };
