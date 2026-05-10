792a7c5c18665b4abd24e4796a0c39a8acb99cfc
choiminjun <chlalswns200@naver.com>
2026-05-10 20:41:43 +0900

    3-7 컴퓨트1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <h1>양방향 바인딩</h1>
-  
-  <input :value="message" @input="message = $event.target.value">
-
-  <textarea v-model="message"></textarea>
-
-  <input type="checkbox" v-model="checked">
-
-  <input type="radio" value="male" v-model="gender">
-  <input type="radio" value="female" v-model="gender">
-
-  <h1>message 입력값: {{ message }}</h1>
-  <h1>checked 입력값: {{ checked }}</h1>
-  <h1>gender 입력값: {{ gender }}</h1>
+  <h1>{{ fullName }}</h1>
+  <button @click="remainName">이름 초기화</button>
+  <button @click="changeName">이름 변경</button>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message:"",
-      checked: false,
-      gender: ""
+      firstName: "홍",
+      lastName: "길동"
     };
+  },
+  computed: {
+    fullName() {
+      console.log("Compute 호출")
+      return `${this.firstName} ${this.lastName}`
+    }
+  },
+  methods: {
+    remainName(){
+      console.log("버튼1");
+      this.firstName = "홍";
+    },
+    changeName(){
+      console.log("버튼2")
+      this.firstName = "김";
+    }
   }
 };
 </script>
