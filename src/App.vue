797a9c0296a70680c6104175885f98d22a27bc75
choiminjun <chlalswns200@naver.com>
2026-05-12 22:56:28 +0900

    4-1 라이프사이클

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
-  <input v-model="email" type="text">
-
+  <h1>{{ count }}</h1>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      email: "example@example.com"
+      count: 10
     };
   },
-  watch:{
-    email(newValue,oldValue){
-      console.log("newValue",newValue);
-      console.log("oldValue",oldValue);
-    }
+  /* 생성 단계 라이프사이클 */
+  beforeCreate(){
+    console.log("훅 beforeCreate: 인스턴스 생성 직후 데이터 초기화 전", this.count);
+  },
+  created(){
+    console.log("훅 create: 데이터와 이벤트 모두가 초기화 된 후", this.count);
   }
 };
 </script>
