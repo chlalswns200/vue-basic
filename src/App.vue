ee1bd8207de6cc43aec49b191543837faa1befb4
choiminjun <chlalswns200@naver.com>
2026-05-20 23:09:31 +0900

    5-4 피니아 사용

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import Pinia from './components/ch05/05-01.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <!-- <HelloWorld /> -->
+   <Pinia/>
 </template>
---
+++ src/components/ch05/05-01.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>App.vue</h1>
+    <p>Count: {{ counter }}</p>
+    <p>Double Count: {{ doubleCount }}</p>
+    <p>Double Plus One: {{ doublePlusOne }}</p>
+    <button @click="increment">Increment</button>
+    <button @click="randomizeCounter">Randomize</button>
+  </div>
+</template>
+
+<script>
+import { useStore } from "../../store/index.ts";
+export default {
+  data() {
+    return {
+      counter: useStore(),
+    };
+  },
+  computed:{
+    doubleCount(){
+        return this.counter.doubleCount
+    },
+    doublePlusOne(){
+        return this.counter.doublePlusOne
+    }
+  },
+  methods:{
+    increment(){
+        this.counter.increment();
+    },
+    randomizeCounter(){
+        this.counter.randomizeCounter();
+    }
+  }
+};
+</script>
