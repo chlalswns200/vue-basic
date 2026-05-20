1dee6ff101a0ccef2260ad352f04433dcfcb018c
choiminjun <chlalswns200@naver.com>
2026-05-20 22:42:50 +0900

    5-3 피니아 스토어 세팅

---
+++ src/store/index.ts
@@ -0,0 +1,27 @@
+import { defineStore } from "pinia";
+
+export const useStore = defineStore("main", {
+  state: () => {
+    return {
+      title: "제목",
+      description: "설명",
+      count: 10,
+    };
+  },
+  getters: {
+    doubleCount: (state) => state.count * 2,
+
+    // 명시적으로 타입 작성 필요
+    doublePlusOne(): number {
+      return this.doubleCount + 1;
+    },
+  },
+  actions: {
+    increment() {
+      this.count++;
+    },
+    randomizeCounter() {
+      this.count = Math.round(100 * Math.random());
+    },
+  },
+});
